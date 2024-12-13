b4edfc7f968802a92504969366ebfa241de95f40
basleal22 <solibaslu@gmail.com>
2024-12-13 17:03:09 +0300

    added a function to count the lenght of the headline to utils

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 #function to read the csv file
 def read_csv_file(file_path):
     #read the file
@@ -15,6 +16,20 @@ def read_csv_file(file_path):
     return {"data":data,
     "column_names":column_names,
     "row_count":row_count}
+def count_headline_length(data,mean,median_length, stdev_length,length):
+    #get the length of the headline
+    data=data['headline']
+    length=[len(data) for data in data]
+    #calculate the mean,median,stdev
+    mean=np.mean(length)
+    average_length=np.array(length)
+    median_length = np.median(length)
+    stdev_length=np.std(length)
+    return {"average_length":average_length,"mean":mean,
+    "median_length":median_length,
+    "stdev_length":stdev_length,
+    "length":length}#return the values in a dictionary form
+    
 
 
 
