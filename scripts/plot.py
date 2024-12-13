e4d7b4ce62c3fb1c0335671f7cd445e50da69593
basleal22 <solibaslu@gmail.com>
2024-12-13 23:50:13 +0300

    modified plot.py

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,10 +1,8 @@
 import pandas as pd
 from matplotlib import pyplot as plt
-def plot_stock_data(data,date_column='date', stock_value_column='stock_value',title='stock value over time'):
+def plot_stock_data(data,date_column, stock_value_column,title):
     #plots stock value over time
     data = data.copy()
-    data[date_column]=pd.to_datetime(data[date_column],errors='coerce',format='%Y-%m-%d %H:%M:%S')
-    data=data.dropna(subset=[date_column])
     plt.figure(figsize=(10,6))
     #here the date_column is the x axis and the stock_value_column is the y axis
     plt.plot(data[date_column],data[stock_value_column],label=stock_value_column,color='blue')
