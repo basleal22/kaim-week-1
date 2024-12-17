fa2fb24d69f36203073ba4c1f116021dfa9ae4cb
basleal22 <solibaslu@gmail.com>
2024-12-17 11:34:31 +0300

    modified

--- scripts/utils.py
+++ scripts/utils.py
@@ -25,7 +25,14 @@ class financial_analyzer:
         data['sma_200'] = ta.SMA(data['Close'],timeperiod=200)#simple moving average
         data['rsi'] = ta.RSI(data['Close'],timeperiod=14)#relative strength index
         data['ema'] = ta.EMA(data['Close'],timeperiod=200)#exponential moving average
-        data['macd'] = ta.MACD(data['Close'],fastperiod=12,slowperiod=26,signalperiod=9)#moving average convergence divergence
+        # Unpack MACD values
+        macd, signal, hist = ta.MACD(data['Close'], 
+                                    fastperiod=12, 
+                                    slowperiod=26, 
+                                    signalperiod=9)
+        data['macd'] = macd
+        data['macd_signal'] = signal
+        data['macd_hist'] = hist
         return data
     def calculatemetrics_pynance(ticker,start_date,end_date):
         data = yf.download(ticker, start=start_date, end=end_date)#get data from yfinance
